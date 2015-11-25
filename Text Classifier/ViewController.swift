84cb113fad051796f04b40b8878710398802efd9
Jamone Kelly <xg.monie089@gmail.com>
2015-11-25 06:54:17 -0500

    Initial Commit

---
+++ Text Classifier/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text Classifier
+//
+//  Created by Jamone Alexander Kelly on 11/25/15.
+//  Copyright © 2015 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
