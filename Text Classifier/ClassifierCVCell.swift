abf58eb97996b35600b48047dc6084239778f223
Jamone Kelly <xg.monie089@gmail.com>
2015-11-25 07:25:35 -0500

    - Create Classifier View Controller with labels, text fields, and collection view - Create Classifier Collection View Cell subclass

---
+++ Text Classifier/ClassifierCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  ClassifierCVCell.swift
+//  Text Classifier
+//
+//  Created by Jamone Alexander Kelly on 11/25/15.
+//  Copyright © 2015 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ClassifierCVCell: UICollectionViewCell {
+    
+}
---
+++ Text Classifier/ClassifierVC.swift
@@ -0,0 +1,13 @@
+//
+//  ClassifierVC.swift
+//  Text Classifier
+//
+//  Created by Jamone Alexander Kelly on 11/25/15.
+//  Copyright © 2015 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ClassifierVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+}
